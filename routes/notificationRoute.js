bfcf807c5bebc2e6f76966b4c59801cb4c26c1aa
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-02-20 15:50:10 +0700

    Create notificationRoute.js

---
+++ routes/notificationRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {getNotifications, newNotifications, updateNotifications} = require('../controller/notification')
+
+
+const route = express.Router()
+
+route.get('/notifications', getNotifications)
+route.post('/notifications', newNotifications)
+route.put('/notifications', updateNotifications)
+
+
+
+module.exports = route;
