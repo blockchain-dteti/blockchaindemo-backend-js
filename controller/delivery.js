cfadd634b09d61ec2248b15ee264d9d1885afe7c
AriqAryasuta <ariqaryasuta3@gmail.com>
2023-03-12 08:56:38 +0700

    feat: Integrate with Blockchain

--- controller/delivery.js
+++ controller/delivery.js
@@ -2,9 +2,11 @@ const deliveryOrder = require('../model/deliveryOrderModel')
 const depo = require('../model/depoModel')
 const port = require('../model/portModel')
 const shipping = require('../model/shippingAgency')
+const { ThirdwebSDK } = require("@thirdweb-dev/sdk/evm");
 
 exports.newDO = async (req, res) => {
     const { bl_number, do_number, expiry_date, vessel_name, voyage_number, shipping_agency, notify_party, consignee, shipper, port_of_loading, port_of_discharge, place_of_delivery, size_type, gross_weight, depo_name } = req.body
+    const container = { voyage_number: voyage_number, size_type: size_type, gross_weight: gross_weight, depo_name: depo_name }
     try {
         await deliveryOrder.create({
             bl_number: bl_number,
@@ -19,10 +21,11 @@ exports.newDO = async (req, res) => {
             port_of_loading: port_of_loading,
             port_of_discharge: port_of_discharge,
             place_of_delivery: place_of_delivery,
-            size_type: size_type,
-            gross_weight: gross_weight,
-            depo_name: depo_name
+            container: container
         });
+        const sdk = new ThirdwebSDK("avalanche-fuji");
+        const contract = await sdk.getContract("0x1a9CDeBF5c7434F0B65C6Ea8aedA31506902dF9b");
+        const data = await contract.call("createRequestDO", do_number, shipping_agency, "0x24D5c12a26d13e13014C985B69487d823A61A896", notify_party, shipper, consignee, port_of_loading, port_of_discharge, place_of_delivery, container)
         res.json({ msg: "DO Ditambahkan" })
     } catch (error) {
         console.error(error)
